7916299eb8dfa699f6ab1621f08937cf5b31da17
Jesus Angarita <jes.angar11@gmail.com>
2024-09-27 18:05:43 -0500

    Jesus solucion de problemas con categorias

--- src/Products/products.module.ts
+++ src/Products/products.module.ts
@@ -5,9 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Products } from './products.entity';
 import { Categories } from 'src/categories/categories.entity';
 import { Cart } from 'src/cart/cart.entity';
+import { CategoriesModule } from 'src/categories/categories.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Products, Categories])],
+  imports: [TypeOrmModule.forFeature([Products, Categories]), CategoriesModule],
+
   providers: [ProductsService],
   controllers: [ProductsController],
 })
