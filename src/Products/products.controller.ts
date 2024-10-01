4336a19ed5a5d6bdff297e7603f48b43692299a2
ArianTrovato <fedetrovato21@gmail.com>
2024-10-01 18:20:50 -0300

    Arian, documentacion a las 18:19hs(arg) 1/10/24, descripcion mas detallada y descripcion de rutas en swagger

--- src/Products/products.controller.ts
+++ src/Products/products.controller.ts
@@ -15,7 +15,7 @@ import { AuthGuard } from 'src/guards/auth.guard';
 import { RolesGuard } from 'src/guards/roles.guard';
 import { Roles } from 'src/decorators/roles.decorator';
 import { Role } from 'src/roles.enum';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags("Products")
 @Controller('products')
@@ -23,6 +23,12 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
+  @ApiOperation({
+    summary: "Obtener todos los productos",
+    description: `
+      Esta ruta busca todos los productos disponibles en la base de datos.
+    `
+  })
   getProducts(@Query('page') page: number, @Query('limit') limit: number) {
     if (page && limit) {
       return this.productsService.getProducts(page, limit);
@@ -31,22 +37,52 @@ export class ProductsController {
   }
 
   @Get('name/:nombre')
+  @ApiOperation({
+    summary: "Obtener un producto por nombre",
+    description: `
+      Esta ruta busca la información detallada del producto específico mediante su nombre.
+      Se espera recibir el nombre del producto en el parámetro de ruta 'nombre'.
+    `
+  })
   getProductByName(@Param('nombre') nombre: string) {
     return this.productsService.getProductByName(nombre);
   }
 
-  @Get(':category/:category')
+  @Get('category/:category')
+  @ApiOperation({
+    summary: "Obtener productos por categoría",
+    description: `
+      Esta ruta busca la información de los productos que pertenecen a una categoría específica.
+      Se espera recibir la categoría del producto en el parámetro de ruta 'category'.
+    `
+  })
   getProductByCategory(@Param('category') category: string) {
     return this.productsService.getProductByCategory(category);
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: "Obtener producto por ID",
+    description: `
+      Esta ruta busca la información del producto cuyo ID coincida con el proporcionado.
+      Se espera recibir el ID del producto en el parámetro de ruta 'id'.
+    `
+  })
   getProductById(@Param('id') id: string) {
     return this.productsService.getProductById(id);
   }
 
   @Post()
   @ApiBearerAuth()
+  @ApiOperation({
+    summary: "Agregar un nuevo producto",
+    description: `
+      Esta ruta permite agregar un nuevo producto a la base de datos. 
+      Se debe enviar un objeto con los detalles del producto en el cuerpo de la solicitud.
+      La ruta requiere autenticación mediante token Bearer.
+      Solo los usuarios autenticados y autorizados pueden realizar esta operación.
+    `
+  })
   //@Roles(Role.Admin)
   //@UseGuards(AuthGuard, RolesGuard)
   addProduct(@Body() product: Partial<Products>) {
@@ -55,6 +91,14 @@ export class ProductsController {
 
   @Put(':id')
   @ApiBearerAuth()
+  @ApiOperation({
+    summary: "Editar un producto existente",
+    description: `
+      Esta ruta permite modificar los detalles de un producto existente, identificado por su ID.
+      Se debe proporcionar el ID del producto en el parámetro de la URL y los cambios a realizar en el cuerpo de la solicitud.
+      La ruta requiere autenticación mediante token Bearer y solo los administradores tienen permiso para realizar esta operación.
+    `
+  })
   @Roles(Role.Admin)
   @UseGuards(AuthGuard, RolesGuard)
   editProduct(@Param('id') id: string, @Body() product: Partial<Products>) {
@@ -63,6 +107,14 @@ export class ProductsController {
 
   @Delete(':id')
   @ApiBearerAuth()
+  @ApiOperation({
+    summary: "Eliminar un producto",
+    description: `
+      Esta ruta permite eliminar un producto de la base de datos, identificado por su ID.
+      Se debe proporcionar el ID del producto en el parámetro de la URL.
+      La ruta requiere autenticación mediante token Bearer y solo los administradores tienen permiso para realizar esta operación.
+    `
+  })
   @Roles(Role.Admin)
   @UseGuards(AuthGuard, RolesGuard)
   deleteProduct(@Param('id') id: string) {
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -3,7 +3,7 @@ import { OrdersService } from './orders.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { Cart } from 'src/cart/cart.entity';
 import { Orders } from './orders.entity';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 
 @ApiTags("Orders")
@@ -14,12 +14,25 @@ export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
+  @ApiOperation({
+    summary: "Crear una nueva orden",
+    description: `
+      Esta ruta permite a los usuarios autenticados crear una nueva orden.
+      Se debe proporcionar un cuerpo con el 'userId' del usuario y una lista de 'products' que serán parte de la orden.
+    `
+  })
   addOrder(@Body() order: any) {
     const { userId, products } = order;
     return this.ordersService.addOrder(userId, products);
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: "Obtener una orden por ID",
+    description: `
+      Esta ruta permite a los usuarios autenticados obtener los detalles de una orden específica utilizando su 'id'.
+    `
+  })
   getOrder(@Query('id') id: string) {
     return this.ordersService.getOrder(id);
   }
